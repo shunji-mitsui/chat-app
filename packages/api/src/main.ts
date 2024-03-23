5f7597c667c1537b7e67a87c6fceb2c741223058
shunji-mitsui <shunji0624mm@gmail.com>
2024-03-23 16:01:35 +0900

    nest.jsのHMR対応

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -9,5 +11,9 @@ async function bootstrap() {
     allowedHeaders: '*', // すべてのヘッダーを許可
   });
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
